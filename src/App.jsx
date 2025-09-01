358ef47a799a71e07ffa792503e464df36b8e452
Ryan Barszcz <rbarszcz@umich.edu>
2025-08-31 20:17:35 -0400

    finished contact and cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from "gsap/all"
+
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Cocktails from './components/Cocktails.jsx'
 import About from './components/About.jsx'
 import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,8 +19,10 @@ const App = () => {
             <Cocktails />
             <About />
             <Art />
+            <Menu />
+            <Contact />
         </main>
-    );
-};
+    )
+}
 
 export default App;
